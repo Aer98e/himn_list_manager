360bde848e7f0878ff1d8b9f728aa8118bb3dfd7
Aaron_P <aaronestebanperezespinal@gmail.com>
2025-04-04 15:42:04 -0500

    Se agrego el concatenador para el datafrmae final

--- moment_1.py
+++ moment_1.py
@@ -30,10 +30,8 @@ def Extraer_Cuadros(file):
                     for col_a in range(-1, 3):
                         try:
                             revise = data_f.iat[i + fila_aum, j + col_a]
-                        except:
-                            print("__________________________________")
-                            print("============ EXEPTION ============")
-                            print("__________________________________")
+                        except IndexError:
+                            print(f"Índice fuera de rango en la fila {i + fila_aum}, columna {j + col_a}")
                             break
                         if revise !=  "" and pd.notna(revise):
                             fila_aum += 1
@@ -126,19 +124,19 @@ def add_titles_stranges(cuadros):
                         cuadro.iat[fila, COL_TIT] = tit_sim
                         break
 
-def _extract_days(cuadros):
-    days = []
-    for cuadro in cuadros:
-        day = cuadro.iat[0, 0]
-        day = str(day)
-        num = re.search(r'\d+', day)
-        if num is not None:
-            days.append(int(num[0]))
-        else:
-            print('Un cuadro no tiene fecha.')
-    return days
-
 def correction_days(cuadros, month = None, year = None):
+    def _extract_days(cuadros):
+        days = []
+        for cuadro in cuadros:
+            day = cuadro.iat[0, 0]
+            day = str(day)
+            num = re.search(r'\d+', day)
+            if num is not None:
+                days.append(int(num[0]))
+            else:
+                print('Un cuadro no tiene fecha.')
+        return days
+
     dates = []
 
     weeken=['LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO']
@@ -147,11 +145,12 @@ def correction_days(cuadros, month = None, year = None):
 
     year = today.year if year is None else year
     month = today.month if month is None else month
+    print(f'año:{year}, mes {month}')
 
-    days_li = _extract_days(cuadros)
+    day_list = _extract_days(cuadros)
     afternoon = False
 
-    for i, day in enumerate(days_li):
+    for i, day in enumerate(day_list):
         table_date = datetime.date(year, month, day)
         if table_date < today:#El igual permitira trabajar ese mismo dia o no.
             dates.append(None)
@@ -163,9 +162,9 @@ def correction_days(cuadros, month = None, year = None):
                 dates.append(f'{temp_date} T')
                 afternoon = False
             
-            elif i < len(days_li)-1:
-                if day == days_li[i+1]:
-                    day == days_li[i+1]
+            elif i < len(day_list)-1:
+                if day == day_list[i+1]:
+                    day == day_list[i+1]
                     dates.append(f'{temp_date} M')
                     afternoon = True
                     continue
@@ -181,18 +180,92 @@ def filter_tables_day(cuadros, corrector):
         if corrector[i] is None:
             continue
         cuadros[i].iat[0, 0] = corrector[i]
-        cuadros_new.append(cuadros[i])
+        cuadros_new.append(cuadros[i].copy())
     return cuadros_new
 
-    
+def concatenate_dataframes(df_list, limit = 3):
+    def _add_empty_columns(df_list, limit=3):
+        """
+        Agrupa DataFrames en listas y añade columnas vacías según un límite.
+
+        Args:
+            df_list (list): Lista de DataFrames.
+            limit (int): Número máximo de DataFrames por grupo.
+
+        Returns:
+            list: Lista de listas de DataFrames agrupados.
+        """
+        current_group = []
+        grouped_dataframes = []
+        group_counter = 0
+
+        for i, df in enumerate(df_list):
+            df = df.copy()
+            df.reset_index(drop=True, inplace=True)  # Reinicia el índice del DataFrame
+             # Copia el DataFrame original para evitar modificarlo directamente
+            group_counter+=1
+
+            if i < len(df_list)-1:
+
+                if group_counter < limit:
+                    df['empty'] = None
+                
+                else:
+                    group_counter = 0
+                    current_group.append(df)
+                    grouped_dataframes.append(current_group)
+                    current_group = []
+                    continue
+                
+                current_group.append(df)
+            
+            else:
+                current_group.append(df)
+                grouped_dataframes.append(current_group)
+        
+        return grouped_dataframes
+
+    def _concatenate_column(pack_row):
+        rows_list = []
+
+        for row in pack_row:
+            result = pd.concat(row, axis=1, ignore_index=True)  # Apila las columnas
+            rows_list.append(result)
+
+        return rows_list
+
+    def _concatenate_row(rows_list):
+        empty_row = pd.DataFrame({col:[None] for col in rows_list[0].columns})  # Crea una fila vacía con las mismas columnas que el primer DataFrame
+        with_empty_row = []
+        for i, df in enumerate(rows_list):
+            with_empty_row.append(df)
+            if i < len(rows_list) - 1:
+                with_empty_row.append(empty_row)
+                
+        result = pd.concat(with_empty_row, ignore_index=True)  # Apila las filas
+        return result
+
+    pack_rows = _add_empty_columns(df_list, limit)
+    rows_list = _concatenate_column(pack_rows)
+    # for row in rows_list:
+    #     print(row)
+    #     print('----------------------------------')
+    df_master = _concatenate_row(rows_list)
+    return df_master
+
 
 def main():
     cuadros = Extraer_Cuadros('Himnos 2025 marzo.xlsx')
-    print(cuadros[0])
-    # add_titles_stranges(cuadros)
     result = correction_days(cuadros)
     cuadros_new = filter_tables_day(cuadros, result)
-    del cuadros
+    df_master = concatenate_dataframes(cuadros_new, limit=3)
+
+    df_master = df_master.fillna("-")
+
+    print(df_master)
+
+    
+    
 
 
     
